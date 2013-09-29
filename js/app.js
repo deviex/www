8fa68a9deed45443c4d15bb14934cd309be2fe7d
developerscode <developerscode@yahoo.com>
2013-09-29 20:03:13 +0530

    Tanisha
    Implemented facebook Login button, New css files applied as per new
    Theme, Dropdownlist removed and added listview with filter option

--- js/app.js
+++ js/app.js
@@ -20,39 +20,37 @@ var globalurl = "http://iexperience.in/api/";
 console.log("Enter app router");
 $(document).on("ready", function () {
     app.router = new app.routers.AppRouter();
-    app.utils.templates.load(["fblogin","cities","map","xp"],
+    app.utils.templates.load(["fblogin", "cities", "map", "xp"],
         function () {
             app.router = new app.routers.AppRouter();
             Backbone.history.start();
-			FB.init({ appId: "520455088008607", nativeInterface: CDV.FB, useCachedDialogs: false });
-			
+            //FB.init({ appId: "520455088008607", nativeInterface: CDV.FB, useCachedDialogs: false });
+            FB.init({ appId: "184637508390380", nativeInterface: CDV.FB, useCachedDialogs: false });
         });
-		
-	FB.Event.subscribe('auth.statusChange', function(event) {
-        if (event.status === 'connected') {
-         FB.api('/me', function (response) {
-                app.user = response; // Store the newly authenticated FB user
-            });
-            
-           // fb.slider.removeCurrentPage();
-            app.router.navigate("destination", {trigger: true});
-        } else {
-            fb.user = null; // Reset current FB user
-            fb.router.navigate("", {trigger: true});
-        }
-    });
-	FB.Event.subscribe('auth.logout', function(response) {
-        alert('auth.logout event');
-        });
-           
-	FB.Event.subscribe('auth.sessionChange', function(response) {
-	        alert('auth.sessionChange event');
-	        });
-	           
-	FB.Event.subscribe('auth.statusChange', function(response) {
-	        alert('auth.statusChange event');
-	        });
-		
-}
 
-);
+        FB.Event.subscribe('auth.statusChange', function (event) {
+            if (event.status === 'connected') {
+                FB.api('/me', function (response) {
+                    app.user = response; // Store the newly authenticated FB user
+                });
+
+                fb.slider.removeCurrentPage();
+                app.router.navigate("destination", { trigger: true });
+            } else {
+                fb.user = null; // Reset current FB user
+                fb.router.navigate("", { trigger: true });
+            }
+        });
+        FB.Event.subscribe('auth.logout', function (response) {
+            alert('auth.logout event');
+        });
+
+        FB.Event.subscribe('auth.sessionChange', function (response) {
+            alert('auth.sessionChange event');
+        });
+
+        FB.Event.subscribe('auth.statusChange', function (response) {
+            alert('auth.statusChange event');
+        });
+    });
+   
